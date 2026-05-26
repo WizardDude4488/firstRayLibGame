4c31713d6e5562748e54f5ca78e5727df7a794ce
WizardDude4488 <samuelhitt48@gmail.com>
2026-05-26 16:04:18 -0400

    Fixed toolchain issues, which were caused by a combination of bad triple declaration in CMake and probably some UB from CMAKE_PREFERRED_(blah) command.

--- main.cpp
+++ main.cpp
@@ -1,2 +1,49 @@
 #include <iostream>
+#include <string.h>
 #include "raylib.h"
+using namespace std;
+
+int screen_width = 1366;
+int screen_height = 768;
+
+class Player {
+    public:
+        void move() {
+            int position_x = screen_width/2;
+            int position_y = screen_height/2;
+            if (IsKeyDown(KEY_RIGHT))
+            {
+                position_x += 5;
+            } if (IsKeyDown(KEY_LEFT))
+            {
+                position_x -= 5;
+            } if (IsKeyDown(KEY_UP))
+            {
+                position_y -= 5;
+            } if (IsKeyDown(KEY_DOWN))
+            {
+                position_y += 5;
+            }
+        }
+};
+
+
+int main()
+{
+    //InitWindow function needs a constant character pointer to a title
+    const char *title;
+    title = "A";
+
+    InitWindow(screen_width, screen_height, title);
+    ClearBackground(WHITE);
+    BeginDrawing();
+    //BeginMode2D();
+
+    Player playerObj;
+
+    //keep window open until user input tells game to close
+    while (WindowShouldClose() == false) {
+        playerObj.move();
+    }
+
+}
