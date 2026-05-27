7e5e21e52bf3345b4522942c6028d3fa7661e93c
WizardDude4488 <samuelhitt48@gmail.com>
2026-05-27 09:40:55 -0400

    Working on window setup.

--- main.cpp
+++ main.cpp
@@ -1,49 +1,34 @@
 #include <iostream>
 #include <string.h>
 #include "raylib.h"
-using namespace std;
-
-int screen_width = 1366;
-int screen_height = 768;
-
-class Player {
-    public:
-        void move() {
-            int position_x = screen_width/2;
-            int position_y = screen_height/2;
-            if (IsKeyDown(KEY_RIGHT))
-            {
-                position_x += 5;
-            } if (IsKeyDown(KEY_LEFT))
-            {
-                position_x -= 5;
-            } if (IsKeyDown(KEY_UP))
-            {
-                position_y -= 5;
-            } if (IsKeyDown(KEY_DOWN))
-            {
-                position_y += 5;
-            }
-        }
-};
 
 
 int main()
 {
-    //InitWindow function needs a constant character pointer to a title
-    const char *title;
-    title = "A";
+    InitWindow(1366, 768, "Raze");
+    SetTargetFPS(60);
+    SetWindowPosition(400, 400);
 
-    InitWindow(screen_width, screen_height, title);
-    ClearBackground(WHITE);
-    BeginDrawing();
-    //BeginMode2D();
+    //add the assets file directory to the project so we can use images, sounds, etc.
+    //ChangeDirectory(TextFormat("%s/../assets/images", GetApplicationDirectory()));
 
-    Player playerObj;
+    //check if window is being created
+    if (IsWindowReady()) printf("Window is Ok\n");
+    else printf("Window failed\n");
+
 
     //keep window open until user input tells game to close
-    while (WindowShouldClose() == false) {
-        playerObj.move();
+    while (!WindowShouldClose()) {
+
+        printf("Loop iteration\n"); fflush(stdout);
+        BeginDrawing();
+        printf("After begin drawing\n"); fflush(stdout);
+            ClearBackground(BLACK);
+            DrawText("It Works!", 24, 24, 20, WHITE);
+        EndDrawing();
+        printf("After end drawing\n"); fflush(stdout);
     }
 
+    CloseWindow();
+    return 0;
 }
