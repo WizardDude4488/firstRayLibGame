703638fed8cb7e96157578423b1e7f0406f8f362
WizardDude4488 <samuelhitt48@gmail.com>
2026-05-30 00:08:37 -0400

    Played around with some changes to the direction calculation in the player class to try and fix the window resizing problem.

--- main.cpp
+++ main.cpp
@@ -5,6 +5,13 @@
 #include <cstdio>
 #include <algorithm>
 
+//creating variables here for global functionality
+float offsetX;
+float offsetY;
+
+float scale;
+
+Camera2D playerCamera = {};
 
 class Player
 {
@@ -19,6 +26,7 @@ public:
     Texture2D player = LoadTexture("images/players blue x3.png");
     Rectangle playerIdleSrc = {32,48,32,48};
     float angle = 0;
+
 
     Player(float x, float y)
     {
@@ -47,7 +55,11 @@ public:
         position = Vector2Add(position, Vector2Scale(dir, (speed * dt)));
 
         //update mouse, rects, etc.
-        mouse = GetMousePosition();
+        mouse = GetScreenToWorld2D(GetMousePosition(), playerCamera);
+
+        //adjust mouse position based on the offset of the screen
+        //do this after GetScreenToWorld2D since the window offsets are calculated last
+
         dst = {position.x, position.y, 32, 48};
 
         //update rotation of player based on updated mouse and dst
@@ -56,6 +68,8 @@ public:
         //calculate a new angle for the player rectangle to rotate to
         angle = atan2f(mouseDir.y, mouseDir.x) * RAD2DEG;
         std::cout << angle << std::endl;
+        //std::cout << mouseDir.x << std::endl;
+        //std::cout << mouseDir.y << std::endl;
 
         //draw player
         DrawTexturePro(player, playerIdleSrc, dst, origin, angle, WHITE);
@@ -80,8 +94,7 @@ int main()
 
     //player
     Player playerObj(Base_W/2, Base_H/2);
-    Camera2D playerCamera = {};
-    playerCamera.zoom = 0.5f;
+    playerCamera.zoom = 2.0f;
 
     Texture2D player = LoadTexture("players blue x3.png");
 
@@ -96,27 +109,31 @@ int main()
 
     //canvas
     RenderTexture2D canvas = LoadRenderTexture(Base_W, Base_H);
+    SetTextureFilter(canvas.texture, TEXTURE_FILTER_TRILINEAR);
 
     //keep window open until user input tells game to close
     while (!WindowShouldClose()) {
 
         //Begin texture mode for drawing to canvas
         BeginTextureMode(canvas);
-
-        //begin 2D mode with player camera
+        //begin 2D mode with player camera,
+        BeginMode2D(playerCamera);
             ClearBackground(BLACK);
             DrawTexture(background, 0, 0, WHITE);
             DrawText("It Works!", 24, 24, 20, WHITE);
             playerObj.update_pose();
+            playerCamera.target = playerObj.position;
+            playerCamera.offset = {Base_W/2, Base_H/2};
+        EndMode2D();
         EndTextureMode();
 
-        float scale = std::min(
+        scale = std::min(
             (float)GetScreenWidth()/Base_W,
             (float)GetScreenHeight()/Base_H
         );
 
-        float offsetX = (GetScreenWidth() - Base_W * scale) * 0.5f;
-        float offsetY = (GetScreenHeight() - Base_H * scale) * 0.5f;
+        offsetX = (GetScreenWidth() - Base_W * scale) * 0.5f;
+        offsetY = (GetScreenHeight() - Base_H * scale) * 0.5f;
 
         Rectangle src = {0,0, (float)Base_W, -(float)Base_H};
         Rectangle dest = {offsetX, offsetY, (float)Base_W * scale, (float)Base_H * scale};
